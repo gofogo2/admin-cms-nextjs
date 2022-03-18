729e1eba9e9450ac5a9be39835178b6993c76d93
gofogo2 <promoted.go@gmail.com>
2022-03-19 03:44:15 +0900

    add: 히스토리 테이블완성
    add: 히스토리 테이블완성

--- pages/contentSearch.tsx
+++ pages/contentSearch.tsx
@@ -6,9 +6,12 @@ import { NextPage } from "next";
 import React, { useEffect, useState } from "react";
 import HistoryContent from "@components/HistoryContent";
 import {
+  Button,
   CircularProgress,
   Dialog,
+  DialogActions,
   DialogContent,
+  DialogContentText,
   DialogTitle,
   LinearProgress,
   Paper,
@@ -25,33 +28,43 @@ import TableHeader from "@components/TableHeader";
 
 const ContentSearch: NextPage = () => {
   const columns = [
-    { id: "seq", label: "Seq", minWidth: 170 },
-    { id: "period", label: "period", minWidth: 100 },
+    { id: "seq", label: "Seq", minWidth: 30 },
+    { id: "id", label: "ID", minWidth: 30 },
+    { id: "period", label: "Period", minWidth: 50 },
     {
       id: "content",
       label: "Content",
-      minWidth: 170,
-      align: "right",
+      align: "left",
     },
   ];
 
-  function createData(seq: string, period: string, content: string) {
-    return { seq, period, content };
+  function createData(
+    seq: string,
+    id: string,
+    period: string,
+    content: string
+  ) {
+    return { seq, id, period, content };
   }
 
   const rows = [
-    createData("1", "1920", "블라블라블라블라블라"),
-    createData("2", "1930", "sdsdfsdf"),
-    createData("3", "1940", "cvbvcbcvb"),
-    createData("4", "1950", "ttwerwerwer"),
-    createData("5", "1960", "fghdfkbjds"),
-    createData("6", "1970", "ewrerwrwerwer"),
-    createData("7", "1980", "fgdgdfgdfg"),
-    createData("8", "1990", "werwerrwew"),
-    createData("9", "2000", "dfgfdgdfgfdgdfg"),
-    createData("10", "2010", "vcbvcbvvcbcvb"),
-    createData("11", "2020", "dgdfgdfgfdgdfgdfg"),
-    createData("12", "2022", "werwerwerwerwerwer"),
+    createData(
+      "1",
+      "1",
+      "1920",
+      "베베베베베베베베베베베베베베베베베베베베베베베베베베베베베베베"
+    ),
+    createData("2", "2", "1930", "sdsdfsdf"),
+    createData("3", "3", "1940", "cvbvcbcvb"),
+    createData("4", "4", "1950", "ttwerwerwer"),
+    createData("5", "5", "1960", "fghdfkbjds"),
+    createData("6", "6", "1970", "ewrerwrwerwer"),
+    createData("7", "7", "1980", "fgdgdfgdfg"),
+    createData("8", "8", "1990", "werwerrwew"),
+    createData("9", "9", "2000", "dfgfdgdfgfdgdfg"),
+    createData("10", "10", "2010", "vcbvcbvvcbcvb"),
+    createData("11", "11", "2020", "dgdfgdfgfdgdfgdfg"),
+    createData("12", "12", "2022", "werwerwerwerwerwer"),
   ];
 
   const [page, setPage] = React.useState(0);
@@ -66,53 +79,96 @@ const ContentSearch: NextPage = () => {
     setPage(0);
   };
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const click = (e: React.FormEvent<HTMLInputElement>) => {};
+
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, i) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={'row'+ i + 1}>
-                    {columns.map((column,j) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={j + 1} align={column.align}>
-                          {value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <div>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, i) => {
+                  return (
+                    <TableRow
+                      onClick={(e) => handleClickOpen()}
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={"row" + i + 1}
+                    >
+                      {columns.map((column, j) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={j + 1} align={column.align}>
+                            {value.length < 10
+                              ? value
+                              : `${value.substring(0, 20)}...`}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <Dialog open={open}>
+        <DialogTitle>수정</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            히스토리 콘텐츠의 변경 내용을 입력해주세요
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="content"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} >취소</Button>
+          <Button  onClick={handleClickOpen} >수정</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 export default ContentSearch;
